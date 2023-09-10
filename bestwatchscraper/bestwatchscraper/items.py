cfbf6a2688e143ec1cf894fd6d95a9a22a0583a1
Lulu Tung <lulutheflaneur@gmail.com>
2023-09-09 20:52:23 -0400

    Use Scrapy Item for validation

--- bestwatchscraper/bestwatchscraper/items.py
+++ bestwatchscraper/bestwatchscraper/items.py
@@ -8,5 +8,74 @@ import scrapy
 
 class BestwatchscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
+
+
+class WatchItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    sku = scrapy.Field()
+    currency = scrapy.Field()
+    price = scrapy.Field()
+    watch_spec = scrapy.Field()
+
+
+# Unused below
+class WatchSpecItem(scrapy.Item):
+    information = scrapy.Field()
+    dial = scrapy.Field()
+    case = scrapy.Field()
+    band = scrapy.Field()
+    movement = scrapy.Field()
+    features = scrapy.Field()
+
+
+class WatchSpecInfoItem(scrapy.Item):
+    brand = scrapy.Field()
+    series = scrapy.Field()
+    model = scrapy.Field()
+    gender = scrapy.Field()
+    produced = scrapy.Field()
+    limited = scrapy.Field()
+
+
+class WatchSpecDialItem(scrapy.Item):
+    dial_type = scrapy.Field()
+    dial_color = scrapy.Field()
+    finish = scrapy.Field()
+    indexes = scrapy.Field()
+    hands = scrapy.Field()
+
+
+class WatchSpecCaseItem(scrapy.Item):
+    material = scrapy.Field()
+    bezel = scrapy.Field()
+    glass = scrapy.Field()
+    back = scrapy.Field()
+    shape = scrapy.Field()
+    diameter = scrapy.Field()
+    height = scrapy.Field()
+    lug_width = scrapy.Field()
+
+
+class WatchSpecBandItem(scrapy.Item):
+    band_material = scrapy.Field()
+    band_color = scrapy.Field()
+
+
+class WatchSpecMovementItem(scrapy.Item):
+    caliber = scrapy.Field()
+    type = scrapy.Field()
+    diameter = scrapy.Field()
+    jewels = scrapy.Field()
+    reserve = scrapy.Field()
+    frequency = scrapy.Field()
+    time = scrapy.Field()
+    chronograph = scrapy.Field()
+    additionals = scrapy.Field()
+
+
+class WatchSpecFeatureItem(scrapy.Item):
+    water_resistance = scrapy.Field()
+    watch_features = scrapy.Field()
--- bestwatchscraper/bestwatchscraper/spiders/bestwatchspider.py
+++ bestwatchscraper/bestwatchscraper/spiders/bestwatchspider.py
@@ -1,10 +1,12 @@
-import re
 import scrapy
+from bestwatchscraper.items import WatchItem, WatchSpecItem, WatchSpecInfoItem, WatchSpecDialItem, WatchSpecCaseItem, \
+    WatchSpecBandItem, WatchSpecMovementItem, WatchSpecFeatureItem
 
 
 class BestwatchspiderSpider(scrapy.Spider):
     name = 'bestwatchspider'
     allowed_domains = ['bestwatch.com.hk']
+
     # start_urls = ['http://bestwatch.com.hk/']
 
     def start_requests(self):
@@ -22,21 +24,53 @@ class BestwatchspiderSpider(scrapy.Spider):
             yield response.follow(next_page_url, callback=self.parse)
 
     def parse_watch_page(self, response):
-        watch_data = {
-            'url': response.url,
-            'title': response.css('span.product-item-name::text').get(),
-            'sku': response.css('span.product-item-sku::text').get(),
-            'currency': response.css('span.price::text').get().split('$')[0],
-            'price': response.css('span.price::text').get().split('$ ')[1].replace(',', ''),
-            'watch_spec': {}
-        }
+        watch_item = WatchItem()
+        watch_item['url'] = response.url
+        watch_item['title'] = response.css('span.product-item-name::text').get()
+        watch_item['sku'] = response.css('span.product-item-sku::text').get()
+        watch_item['currency'] = response.css('span.price::text').get().split('$')[0]
+        watch_item['price'] = response.css('span.price::text').get().split('$ ')[1].replace(',', '')
+        watch_item['watch_spec'] = WatchSpecItem()
+
+        # watch_spec_data = {}
+
+        # watch_data = {
+        #     'url': response.url,
+        #     'title': response.css('span.product-item-name::text').get(),
+        #     'sku': response.css('span.product-item-sku::text').get(),
+        #     'currency': response.css('span.price::text').get().split('$')[0],
+        #     'price': response.css('span.price::text').get().split('$ ')[1].replace(',', ''),
+        #     'watch_spec': {}
+        # }
         table_cols = response.css('.tbw-row > .cols')
         for table_col in table_cols:
-            col_subtitle = table_col.css('.table-title::text').get().strip('\n').strip()
+            col_subtitle = table_col.css('.table-title::text').get().strip('\n').strip().lower()
+            watch_spec_category = WatchSpecItem
+
+            match col_subtitle:
+                case 'information':
+                    watch_item['watch_spec'][col_subtitle] = WatchSpecInfoItem()
+                case 'dial':
+                    watch_item['watch_spec'][col_subtitle] = WatchSpecDialItem()
+                case 'case':
+                    watch_item['watch_spec'][col_subtitle] = WatchSpecCaseItem()
+                case 'band':
+                    watch_item['watch_spec'][col_subtitle] = WatchSpecBandItem()
+                case 'movement':
+                    watch_item['watch_spec'][col_subtitle] = WatchSpecMovementItem()
+                case 'features':
+                    watch_item['watch_spec'][col_subtitle] = WatchSpecFeatureItem()
+
+            # watch_item['watch_spec'][col_subtitle] = watch_spec_item
             subtables = table_col.css('ul.table > li')
             for subtable in subtables:
-                subtable_label = subtable.css('.table__label::text').get().strip('\n').strip()
+                subtable_label = subtable.css('.table__label::text').get().strip('\n').strip().lower().replace(' ', '_')
                 subtable_value = subtable.css('.table__context::text').get().strip('\n').strip()
+                watch_item['watch_spec'][col_subtitle][subtable_label] = subtable_value
                 # watch_data['watch_spec'][col_subtitle][subtable.css('.table__label::text').get()] = subtable.css('.table__context::text').get()
-                watch_data.setdefault('watch_spec', {}).setdefault(col_subtitle, {}).setdefault(subtable_label, subtable_value)
-        yield watch_data
+                # watch_spec_data.setdefault(col_subtitle, {}).setdefault(subtable_label, subtable_value)
+                # watch_spec_item = subtable_value
+
+        # watch_item['watch_spec'] = watch_spec_item
+
+        yield watch_item
